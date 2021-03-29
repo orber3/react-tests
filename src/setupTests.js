96c807573b0f118b010343250147ff7ce0a36806
or berman <orber3@gmail.com>
2021-03-29 10:16:29 +0300

    basic setup

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,14 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import Enzyme from 'enzyme'
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17'
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+
+
+Enzyme.configure({ 
+    adapter: new Adapter() ,
+    disableLifecycleMethods: true
+
+});
